1c42c5716696d788f1a6326b43fc61ff144e70be
AR621 <60547627+AR621@users.noreply.github.com>
2021-10-29 20:08:31 +0200

    Initial commit

---
+++ app/src/main/java/JandD/zinify/FilterChoiceActivity.kt
@@ -0,0 +1,11 @@
+package JandD.zinify
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FilterChoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.filter_choice)
+    }
+}
